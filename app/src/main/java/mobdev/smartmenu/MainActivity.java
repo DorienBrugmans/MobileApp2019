51ac2ee1fc99631dea34ae90497af4dbc4dc0337
caglarcelikoz <caglarcelikoz@outlook.be>
2018-10-02 14:11:43 +0200

    ok

--- app/src/main/java/mobdev/smartmenu/MainActivity.java
+++ app/src/main/java/mobdev/smartmenu/MainActivity.java
@@ -2,12 +2,21 @@ package mobdev.smartmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate: started.");
+        ImageView firstImage=(ImageView) findViewById(R.id.firstImage);
+
+        int imageResource=getResources().getIdentifier("@drawable/desert",null,this.getPackageName());
+        firstImage.setImageResource(imageResource);
+
     }
 }
