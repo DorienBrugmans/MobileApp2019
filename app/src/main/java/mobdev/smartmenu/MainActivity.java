0aa182cfdd969e56271561be606e2791f5039bdb
caglarcelikoz <caglarcelikoz@outlook.be>
2018-10-02 14:48:04 +0200

    fix

--- app/src/main/java/mobdev/smartmenu/MainActivity.java
+++ app/src/main/java/mobdev/smartmenu/MainActivity.java
@@ -7,15 +7,9 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG="MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"onCreate: started.");
-        ImageView firstImage=(ImageView) findViewById(R.id.image);
-
-        int imageResource=getResources().getIdentifier("@assets/foto",null,this.getPackageName());
-        firstImage.setImageResource(imageResource);
     }
 }
